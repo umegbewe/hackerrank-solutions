816d4ddcb126b53a7b8876e00c42db10f22cf1fc
Umegbewe Nwebedu <nwebedujunior55@gmail.com>
2021-07-19 03:54:59 +0100

    Create Awk -3.sh

---
+++ Bash/grep-sed-awk/Awk -3.sh
@@ -0,0 +1,6 @@
+awk '{ sum=$2+$3+$4; avg=sum/3;
+        if (avg>=80) grade="A"
+        else if (avg>=60) grade ="B"
+        else if (avg>=50) grade = "C"
+        else grade="FAIL";
+        print $0, ":", grade; }'
